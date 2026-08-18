b96e82c95cec96399f3799991fe23198ebdce602
Anirudh Raghuvanshi <107320087+anirudh-000@users.noreply.github.com>
2026-08-18 06:34:01 +0000

    Comp. Lesson Data Types

---
+++ Basics/data-types.js
@@ -0,0 +1,31 @@
+"use strict"; // treats all JS code as modern and prevents accidental errors
+
+// alert("Hello World!"); // create an error on terminal coz of using node JS 
+
+let name = "Anirudh";
+let age = 21;
+let isLoggedIn = false;
+let state = null;
+
+// Data types in JS
+
+// primitive data types
+
+// number => 2 to power of 53 - 1
+// bigint 
+// string => "Anirudh" or 'Anirudh' or `Anirudh`
+// boolean => true or false
+// null => standalone value that represents nothing
+// undefined => variable that has not been assigned a value yet
+// symbol => unique and immutable primitive value
+
+// non-primitive data types
+
+//object => collection of key-value pairs
+
+console.log (typeof null); // object
+console.log (typeof undefined); // undefined
+
+
+
+
