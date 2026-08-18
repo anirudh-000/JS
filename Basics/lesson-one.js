d7db2c63eac3f0d92f18f12e846b3920303328e1
Anirudh Raghuvanshi <107320087+anirudh-000@users.noreply.github.com>
2026-08-18 06:15:30 +0000

    Completed Lesson One Variables

--- Basics/lesson-one.js
+++ Basics/lesson-one.js
@@ -1,1 +1,17 @@
-console.log("Anirudh")
+const accountId = 144553
+let accountEmail = "anirudh@example.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+let accountState;
+
+//accountId = 12  // not allowed because accountId is a constant
+
+//console.log(accountId);
+
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState});
+
+
+/*
+prefer not to use var because it is function scoped and can lead to unexpected behavior. Use let and const instead for block scoping. 
+*/
+
