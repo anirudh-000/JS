6e1c371784e32df9d2b8b575375f73f945f59cfc
Anirudh Raghuvanshi <107320087+anirudh-000@users.noreply.github.com>
2026-08-19 04:17:34 +0000

    Comp. Objects

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -57,4 +57,69 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userFullName.firstName); // Anirudh 
+// console.log(regularUser.fullname.userFullName.firstName); // Anirudh 
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2) // merging obj1 and obj2 into a new object
+
+const obj3 = {...obj1, ...obj2} // merging obj1 and obj2 into a new object using spread operator
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "anirudh@example.com"
+    },
+    {
+        id: 2,
+        email: "anirudh2@example.com"
+    },
+    {
+        id: 3,
+        email: "anirudh3@example.com"
+    },
+    {
+        id: 4,
+        email: "anirudh4@example.com"
+    }
+]
+
+users[1].email
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser)); // returns an array of keys of the object
+
+// console.log(Object.values(tinderUser)); // returns an array of values of the object
+
+// console.log(Object.entries(tinderUser)); // returns an array of key-value pairs of the object
+
+// console.log(tinderUser.hasOwnProperty("name")); // returns true if the object has the specified property, false otherwise
+
+const course ={
+    name: "JavaScript",
+    price: 999,
+    courseInstructor: "Anirudh",
+    courseContent: ["JavaScript Basics", "JavaScript Advanced", "JavaScript Frameworks"]
+}
+// course.courseInstructor // to access the value of courseInstructor property
+
+const {courseInstructor} = course // destructuring the course object to get the value of courseInstructor property
+
+const {courseInstructor: instructor} = course // renaming the variable while destructuring 
+
+
+console.log(courseInstructor); // Anirudh
+console.log(instructor); // Anirudh
+
+//json
+
+// {
+//     "name": "ANirudh",
+//     "price": "free",
+//     "courseInstructor": "Anirudh",
+// }
