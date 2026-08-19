10f980acd7973d5f2be73f64f3d86799ccff21fa
Anirudh Raghuvanshi <107320087+anirudh-000@users.noreply.github.com>
2026-08-19 06:49:10 +0000

    Comp. Arrow Functions

---
+++ 03_basics/03_arrow-functions.js
@@ -0,0 +1,52 @@
+// const user = {
+//     username: "Anirudh",
+//     price: 999,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username} , welcome to our website.`)
+//         console.log(this)
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this) // in node env. this represents an empty object
+
+
+// const chai = function () {
+//     let username = "Anirudh"
+//     console.log(this.username)
+// }
+
+const chai = () => {
+    let username = "Anirudh"
+    console.log(this)
+}
+
+// chai()
+
+// normal arrow function (explicit)
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+// implicit return arrow function
+
+// const addTwo = (num1, num2) => num1 + num2
+
+const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "Anirudh"})
+
+
+console.log(addTwo(3, 5))
+
+
+const myArray = [2, 3, 4, 5]
+
+// myArray.forEach(function() {})
+// myArray.forEach(() => {})
+    
