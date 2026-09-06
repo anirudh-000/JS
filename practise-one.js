6b2a8f5f5ae2277b08c135c1660b4d047859d0d7
Anirudh Raghuvanshi <107320087+anirudh-000@users.noreply.github.com>
2026-09-06 06:12:38 +0000

    Practise Done

--- practise-one.js
+++ practise-one.js
@@ -1,24 +1,42 @@
 // Isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
-function isIsogram(word){
-    if (typeof word !== "string"){
-        return `Not a String`
-    } 
+// function isIsogram(word){
+//     if (typeof word !== "string"){
+//         return `Not a String`
+//     } 
 
-    const sorted = word.toLowerCase().split('').sort()
+//     const sorted = word.toLowerCase().split('').sort()
             
-    for(let i=0; i < sorted.length-1; i++ ){
-        if(sorted[i] === sorted[i + 1]){
-        console.log(sorted[i]);
-        console.log(sorted[i+1]);
-        console.log(sorted);
-        return "Not a Isogram"
-        }
-    }
-    console.log(sorted);
-    return "Yes, It's an Isogram";
-}
+//     for(let i=0; i < sorted.length-1; i++ ){
+//         if(sorted[i] === sorted[i + 1]){
+//         console.log(sorted[i]);
+//         console.log(sorted[i+1]);
+//         console.log(sorted);
+//         return "Not a Isogram"
+//         }
+//     }
+//     console.log(sorted);
+//     return "Yes, It's an Isogram";
+// }
 
-console.log(isIsogram("moOse"))
+// console.log(isIsogram("moOse"))
+
+
+// Write code to convert the string '123' into a number and store it in a variable named value.
+
+
+// const string = "123"
+
+// let num = Number(string)
+
+// console.log(num, typeof(num));
+
+
+// const num2 = 123
+
+// let text = num.toString()
+
+// console.log(text, typeof(text));
+
 
 
